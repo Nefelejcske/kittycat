488f3dfe9dbc7357171e3c3af8a6ff21c3c7cebb
Nefelejcske <33089316+Nefelejcske@users.noreply.github.com>
2020-10-26 21:24:27 +0100

    Update vector.c

--- vector.c
+++ vector.c
@@ -8,6 +8,7 @@ void sum_many(int* a, int* b, int* c, int len)
 void sum_many_bad(int* a, int* b, int* c, int* len)
 {
   // Loop can not be vectorized
+  // because of aliasing. i may point to the c arraz
   for(int i = 0; i < *len; ++i)
     c[i] = a[i] + b[i];
 }
